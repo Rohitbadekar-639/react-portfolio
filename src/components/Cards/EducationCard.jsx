06dc45e396e48aceecb236066487d3258b20ba44
Rohit Badekar <rohitbadekar555@gmail.com>
2024-06-18 01:50:49 +0530

    updated components

--- src/components/Cards/EducationCard.jsx
+++ src/components/Cards/EducationCard.jsx
@@ -52,7 +52,7 @@ const Card = styled.div`
     @media only screen and (max-width: 768px){
         padding: 10px;
         gap: 8px;
-        width: 280px;
+        width: 270px;
     }
 
     &:hover ${Document}{
